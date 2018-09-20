c54f26a8fbf34faa214a0d9035db3552dbcbcfd0
Erik Sommer <Erik.Sommer@slub-dresden.de>
2018-09-20 15:25:27 +0200

    Add custom js

---
+++ src/js/custom.js
@@ -0,0 +1,22 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
+import $ from 'jquery';
+
+$(window).on("load", function() {
+    $.ready.then(function() {
+        if ($('#editForm\\:workflowTabView\\:btnReadXmlDiagram').length > 0) {
+            $('#editForm\\:workflowTabView\\:btnReadXmlDiagram')[0].click();
+        } else {
+            $('#js-create-diagram')[0].click();
+        }
+    });
+});
