9f8cb61dff38bb99d9e6b2192b9a050db2a8d707
Erik Sommer <Erik.Sommer@slub-dresden.de>
2018-11-23 15:41:37 +0100

    Remove sequence flow properties
    Those are not available anymore at this location but we don’t use them anyway? Maybe? Not sure about this change yet…

--- src/provider/template/TemplatePropertiesProvider.js
+++ src/provider/template/TemplatePropertiesProvider.js
@@ -9,7 +9,7 @@ import eventProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/EventPr
 import linkProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/LinkProps';
 import idProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/IdProps';
 import nameProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/NameProps';
-import sequenceFlowProps from 'bpmn-js-properties-panel/lib/provider/camunda/parts/SequenceFlowProps';
+
 
 // Require your custom property entries.
 import scriptTaskProperties from './parts/ScriptTaskProperties';
@@ -36,7 +36,6 @@ function createGeneralTabGroups(element, bpmnFactory, elementRegistry, translate
     };
     linkProps(detailsGroup, element, translate);
     eventProps(detailsGroup, element, bpmnFactory, elementRegistry, translate);
-    sequenceFlowProps(detailsGroup, element, bpmnFactory, translate);
 
     // Create a group called "Task Properties".
     var taskGroup = {
