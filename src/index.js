01cfdbd0bdc5d7c6246b7abe3f7db51b75498269
Erik Sommer <Erik.Sommer@slub-dresden.de>
2018-11-26 10:17:20 +0100

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -13,8 +13,6 @@ import diagramXML from '../resources/initialDiagram.bpmn';
 
 var container = $('#js-drop-zone');
 
-var saveDiagramFunctionCall;
-
 var bpmnModeler = new BpmnModeler({
   container: '#js-canvas',
   propertiesPanel: {
@@ -131,39 +129,4 @@ $(function() {
     setDiagramXML();
   });
 
-  var downloadLink = $('#js-download-diagram');
-  var downloadSvgLink = $('#js-download-svg');
-
-  $('.buttons a').click(function(e) {
-    if (!$(this).is('.active')) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-  });
-
-  function setEncoded(link, name, data) {
-    var encodedData = encodeURIComponent(data);
-
-    if (data) {
-      link.addClass('active').attr({
-        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
-        'download': name
-      });
-    } else {
-      link.removeClass('active');
-    }
-  }
-
-  var exportArtifacts = debounce(function() {
-
-    saveSVG(function(err, svg) {
-      setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
-    });
-
-    saveDiagram(function(err, xml) {
-      setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
-    });
-  }, 500);
-
-  bpmnModeler.on('commandStack.changed', exportArtifacts);
 });
