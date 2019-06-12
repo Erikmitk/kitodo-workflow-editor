813d80ed910d547a7ba8dc35d9c6bffe2f441a83
Erik Sommer <Erik.Sommer@slub-dresden.de>
2019-06-12 09:51:16 +0200

    Add viewer control functionality to backend

--- src/index.js
+++ src/index.js
@@ -67,24 +67,102 @@ saveDiagramFunctionCall = function saveDiagramAction() {
   document.getElementById('editForm:workflowTabView:xmlDiagram').value = xmlParam + "kitodo-diagram-separator" + svgParam;
 };
 
+var modelerActions = {
 
-
-
-
-
-
-
-
-
-
+  'modeler.toggleFullscreen': function() {
+    var elem = document.querySelector('html');
+    toggleFullScreen(elem);
+  },
+  'modeler.zoomReset': function() {
+    bpmnModeler.get('zoomScroll').reset();
+  },
+  'modeler.zoomIn': function(e) {
+    bpmnModeler.get('zoomScroll').stepZoom(1);
+  },
+  'modeler.zoomOut': function(e) {
+    bpmnModeler.get('zoomScroll').stepZoom(-1);
   }
+};
 
 
+function toggleFullScreen(element) {
 
+  if (!document.fullscreenElement &&
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
 
-// bootstrap diagram functions
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+
 
 $(function() {
+
+  function actionListener(event) {
+    var jsaction = parseActionAttr($(this));
+
+    var name = jsaction.name,
+      action = modelerActions[name];
+
+    if (!action) {
+      throw new Error('no action <' + name + '> defined');
+    }
+
+    event.preventDefault();
+
+    action(event);
+  }
+
+  var delegates = {};
+
+  $('[jsaction]').each(function() {
+
+    var jsaction = parseActionAttr($(this));
+
+    var event = jsaction.event;
+
+    if (!delegates[event]) {
+      $(document.body).on(event, '[jsaction]', actionListener);
+      delegates[event] = true;
+    }
+
+
+    var name = jsaction.name,
+      handler = modelerActions[name];
+
+    if (!handler) {
+      throw new Error('no action <' + name + '> defined');
+    }
+  });
+
+  function parseActionAttr(element) {
+
+    var match = $(element).attr('jsaction').split(/:(.+$)/, 2);
+
+    //jsaction="event:modelerAction"
+    return {
+      event: match[0],
+      name: match[1]
+    };
+  }
 
   $('#editForm\\:workflowTabView\\:js-create-diagram').click(function(e) {
     e.stopPropagation();
